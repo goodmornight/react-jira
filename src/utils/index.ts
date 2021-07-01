c9595e520d4f45bba819e18d0bdfc463f339d5f6
GoodNight <goodnightlilanhui@gmail.com>
2021-07-01 10:37:24 +0800

    chore: 给useDebounce添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -23,7 +23,7 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState(value);
 
   useEffect(() => {
